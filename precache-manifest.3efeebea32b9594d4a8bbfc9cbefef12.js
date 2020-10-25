95212d647ac52929ba8ddb5cc19d7735eb88f49e
joey <joseph66magdaleno@yahoo.com>
2020-10-25 00:08:48 -0700

    Updates

---
+++ precache-manifest.3efeebea32b9594d4a8bbfc9cbefef12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6b8e43910dd5ad0818abc81daee064",
+    "url": "/React-weather-app/index.html"
+  },
+  {
+    "revision": "04124fac549c4c83702d",
+    "url": "/React-weather-app/static/css/main.abcc1d5d.chunk.css"
+  },
+  {
+    "revision": "419aa54790b886ec0fae",
+    "url": "/React-weather-app/static/js/2.5bc51d9c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/React-weather-app/static/js/2.5bc51d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04124fac549c4c83702d",
+    "url": "/React-weather-app/static/js/main.7a68b41b.chunk.js"
+  },
+  {
+    "revision": "caec4c2e6db10c11ea2b",
+    "url": "/React-weather-app/static/js/runtime-main.1a35e58f.js"
+  }
+]);
